35448b9551cf5c4d6a0013ba994a3b00c0c6eab1
Dzenly <alexchem@yandex.ru>
2016-04-18 10:32:14 +0600

    Improving self tests. Inner docs corrections.

--- tests/tia/06_suiteConfig/00_suiteConfig.js
+++ tests/tia/06_suiteConfig/00_suiteConfig.js
@@ -6,12 +6,7 @@ var util = require('util');
 t.setTitle('Checking config and suite-config work.');
 
 gT.a.equal(gIn.config.configDirDummyOption, 'configDirDummyOption', 'Dir config check');
+gT.a.equal(gIn.config.dummyExpectedRootDirConfigOption, 'dummyExpectedRootDirConfigOption', 'Root dir config check');
+
 gT.a.equal(gT.suiteConfig.dummyGoodSuitConfigOption, 'dummyGoodSuitConfigOption', 'Suite config Expected value check');
-
 gT.a.equal(gT.suiteConfig.dummyBadSuitConfigOption, undefined, 'Suite config Unexpected value check');
-
-
-// t.checkTrue(typeof gT !== 'undefined', 'gT exists');
-// t.checkTrue('config' in gT, 'gIn.config exists');
-// t.checkTrue('tInfo' in gT, 'gIn.tInfo exists');
-// t.checkTrue('data' in gIn.tInfo, 'gIn.tInfo.data exists');
