1e0aea034d45327c3342cf39b6acbbd6d8a3d2b0
Dzenly <alexchem@yandex.ru>
2016-04-20 15:42:14 +0600

    Fix bug with --run-self-tests

--- tests/suite-config.js
+++ tests/suite-config.js
@@ -1,6 +1,12 @@
 'use strict';
 
-var suiteConfig = require('./mail-settings.nogit.json');
+var suiteConfig;
+
+try {
+  suiteConfig = require('./mail-settings.nogit.json');
+} catch(e) {
+
+}
 suiteConfig.dummyGoodSuitConfigOption = 'dummyGoodSuitConfigOption';
 
 module.exports = suiteConfig;
