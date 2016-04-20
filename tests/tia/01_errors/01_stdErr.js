0bc41e915c01164052ff743a1c3b4aafa66734ba
Dzenly <alexchem@yandex.ru>
2016-04-20 12:49:44 +0600

    Minor fix for tests to do not confuse users.

--- tests/tia/01_errors/01_stdErr.js
+++ tests/tia/01_errors/01_stdErr.js
@@ -2,5 +2,6 @@ t.setTitle('TODO (LOW PRIORITY): catch messages from stderr');
 
 l.println('This log should contain message from stderr');
 
-console.warn('This console.warn message should be cought');
-console.error('This console.error message should be cought');
+// TODO:
+// console.warn('This console.warn message should be cought and placed to test log');
+// console.error('This console.error message should be cought and placed to test log');
