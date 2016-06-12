c818b130341087a9713e58526566d6a15713db07
Dzenly <alexchem@yandex.ru>
2016-06-13 01:08:22 +0600

    Fixed a bug with tree and table content printing to log.

--- api/extjs/extjs-content.js
+++ api/extjs/extjs-content.js
@@ -6,7 +6,7 @@
 
 exports.logTableById = function (id, msg, logAction) {
   return gIn.wrap('Logging content of table: "' + msg + '" ... ', logAction, function () {
-    return gT.sOrig.driver.executeScript(`tiaEJ.ctById.get('${id}'`)
+    return gT.sOrig.driver.executeScript(`return tiaEJ.ctById.get('${id}')`)
       .then(function (res) {
         gIn.logger.log(res);
       });
@@ -15,7 +15,7 @@ exports.logTableById = function (id, msg, logAction) {
 
 exports.logTreeById = function (id, msg, logAction) {
   return gIn.wrap('Logging content of tree: "' + msg + '" ... ', logAction, function () {
-    return gT.sOrig.driver.executeScript(`tiaEJ.ctById.getTree('${id}'`)
+    return gT.sOrig.driver.executeScript(`return tiaEJ.ctById.getTree('${id}')`)
       .then(function (res) {
         gIn.logger.log(res);
       });
