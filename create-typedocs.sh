11188e70721e3cca857b394e97b5403240e49f9d
Dzenly <alexchem@yandex.ru>
2019-10-14 19:52:42 +0700

    fix.

--- create-typedocs.sh
+++ create-typedocs.sh
@@ -4,7 +4,7 @@ rm -rf docs
 mkdir docs
 
 #mv readme.md readme.md.bak
-typedoc ./types --out docs --includeDeclarations --excludeExternals --excludeNotExported --readme typedoc-readme.md
+typedoc --out docs --includeDeclarations --excludeExternals --excludeNotExported --readme typedoc-readme.md
 #mv readme.md.bak readme.md
 touch docs/.nojekyll
 
